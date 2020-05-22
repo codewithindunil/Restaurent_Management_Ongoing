fe1d8a711a0cb2584c876c8d102655ba783abc7d
Coding Is Life <codewithindunil@gmail.com>
2020-05-22 14:42:45 +0530

    sql added

--- restauretdb.sql
+++ restauretdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2020 at 04:07 PM
+-- Generation Time: May 22, 2020 at 11:12 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -33,16 +33,19 @@ CREATE TABLE `itemlist_tb` (
   `itemCode` varchar(15) NOT NULL,
   `itemName` varchar(15) NOT NULL,
   `size` varchar(15) NOT NULL,
-  `price` varchar(15) NOT NULL
+  `price` varchar(15) NOT NULL,
+  `availability` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `itemlist_tb`
 --
 
-INSERT INTO `itemlist_tb` (`id`, `itemCode`, `itemName`, `size`, `price`) VALUES
-(1, '1', 'Butter Scotch P', 'Medium', '150'),
-(2, '1', 'Butter Scotch P', 'Large', '250');
+INSERT INTO `itemlist_tb` (`id`, `itemCode`, `itemName`, `size`, `price`, `availability`) VALUES
+(1, '1', 'Butter Scotch P', 'Medium', '150', ''),
+(2, '1', 'Butter Scotch P', 'Large', '250', ''),
+(3, '2', 'test', 'test', 'test', ''),
+(4, '3', 'test', 'test', 'test', '');
 
 -- --------------------------------------------------------
 
@@ -66,37 +69,23 @@ CREATE TABLE `orders_tb` (
 --
 
 INSERT INTO `orders_tb` (`id`, `orderNo`, `itemCode`, `size`, `qty`, `eachPrice`, `total`, `date`) VALUES
-(13, '1', '1', 'Medium', '2', '150', '300', '2312'),
-(20, '2', '1', 'Medium', '1', '150', '150', '3/31/2020 2:36:'),
-(21, '2', '1', 'Medium', '2', '150', '300', '3/31/2020 2:36:'),
-(22, '2', '1', 'Large', '1', '250', '250', '3/31/2020 2:36:'),
-(23, '3', '1', 'Medium', '2', '150', '300', '3/31/2020 3:20:'),
-(24, '4', '1', 'Medium', '2', '150', '300', '3/31/2020 3:24:'),
-(25, '4', '1', 'Large', '2', '250', '500', '3/31/2020 3:25:'),
-(26, '5', '1', 'Medium', '2', '150', '300', '3/31/2020 3:25:'),
-(27, '5', '1', 'Large', '2', '250', '500', '3/31/2020 3:26:'),
-(28, '6', '1', 'Medium', '2', '150', '300', '3/31/2020 3:29:'),
-(29, '6', '1', 'Large', '3', '250', '750', '3/31/2020 3:29:'),
-(30, '7', '1', 'Medium', '2', '150', '300', '3/31/2020 6:38:'),
-(31, '7', '1', 'Large', '3', '250', '750', '3/31/2020 6:39:'),
-(32, '8', '1', 'Medium', '2', '150', '300', '3/31/2020 6:40:'),
-(33, '8', '1', 'Large', '2', '250', '500', '3/31/2020 6:40:'),
-(34, '8', '1', 'Large', '23', '250', '5750', '3/31/2020 6:41:'),
-(35, '9', '1', 'Medium', '2', '150', '300', '3/31/2020 6:41:'),
-(36, '9', '1', 'Large', '1', '250', '250', '3/31/2020 6:42:'),
-(37, '10', '1', 'Large', '2', '250', '500', '3/31/2020 6:44:'),
-(38, '10', '1', 'Large', '2', '250', '500', '3/31/2020 6:44:'),
-(39, '10', '1', 'Medium', '34', '150', '5100', '3/31/2020 6:44:'),
-(40, '10', '1', 'Medium', '2', '150', '300', '3/31/2020 6:49:'),
-(41, '10', '1', 'Medium', '2', '150', '300', '3/31/2020 6:52:'),
-(42, '10', '1', 'Large', '2', '250', '500', '3/31/2020 6:52:'),
-(43, '10', '1', 'Medium', '22', '150', '3300', '3/31/2020 7:00:'),
-(44, '10', '1', 'Medium', '3', '150', '450', '3/31/2020 7:00:'),
-(45, '10', '1', 'Large', '3', '250', '750', '3/31/2020 7:01:'),
-(46, '10', '1', 'Medium', '4', '150', '600', '3/31/2020 7:03:'),
-(47, '10', '1', 'Large', '4', '250', '1000', '3/31/2020 7:03:'),
-(48, '10', '1', 'Medium', '2', '150', '300', '3/31/2020 7:31:'),
-(49, '10', '1', 'Large', '2', '250', '500', '3/31/2020 7:31:');
+(69, '10', '1', 'Large', '2', '250', '500', '4/4/2020 8:03:2'),
+(70, '10', '1', 'Medium', '6', '150', '900', '4/4/2020 8:51:0'),
+(71, '10', '1', 'Medium', '6', '150', '900', '4/4/2020 8:51:1'),
+(72, '10', '1', 'Medium', '1', '150', '150', '5/22/2020 1:01:'),
+(73, '10', '1', 'Medium', '1', '150', '150', '5/22/2020 1:01:'),
+(74, '10', '1', 'Medium', '2', '150', '300', '5/22/2020 1:01:'),
+(75, '10', '1', 'Medium', '2', '150', '300', '5/22/2020 1:01:'),
+(76, '10', '1', 'Medium', '2', '150', '300', '5/22/2020 1:01:'),
+(77, '10', '1', 'Medium', '14', '150', '2100', '5/22/2020 1:16:'),
+(78, '10', '1', 'Medium', '99', '150', '14850', '5/22/2020 1:22:'),
+(79, '10', '1', 'Medium', '123', '150', '18450', '5/22/2020 1:28:'),
+(80, '10', '1', 'Medium', '2', '150', '300', '5/22/2020 2:35:'),
+(81, '10', '1', 'Large', '2', '250', '500', '5/22/2020 2:35:'),
+(82, '10', '1', 'Medium', '3', '150', '450', '5/22/2020 2:36:'),
+(83, '10', '1', 'Large', '4', '250', '1000', '5/22/2020 2:36:'),
+(84, '11', '1', 'Medium', '5', '150', '750', '5/22/2020 2:39:'),
+(85, '11', '1', 'Large', '10', '250', '2500', '5/22/2020 2:39:');
 
 -- --------------------------------------------------------
 
@@ -108,8 +97,17 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(15) NOT NULL,
   `email` varchar(15) NOT NULL,
-  `password` varchar(15) NOT NULL
+  `password` varchar(15) NOT NULL,
+  `privilages` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `privilages`) VALUES
+(1, 'indunil', 'asdasd', '123', ''),
+(2, 'admin', 'admin', 'admin', '1');
 
 --
 -- Indexes for dumped tables
@@ -141,19 +139,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `itemlist_tb`
 --
 ALTER TABLE `itemlist_tb`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `orders_tb`
 --
 ALTER TABLE `orders_tb`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
